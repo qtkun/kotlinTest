3f3764f6a22ab37245beb5c80703b87f128fdb73
qtkun <qutkun@gmail.com>
2023-06-29 16:53:27 +0800

    ViewGroup初始化ViewBinding方法

--- app/src/main/java/com/qtk/kotlintest/extensions/ViewBindingExtensions.kt
+++ app/src/main/java/com/qtk/kotlintest/extensions/ViewBindingExtensions.kt
@@ -4,12 +4,14 @@ import android.app.Activity
 import android.app.Dialog
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.MainThread
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.*
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
+
 @MainThread
 inline fun<reified VB: ViewBinding> Activity.viewBinding(setContentView: Boolean = true) =
     ViewBindingLazy(VB::class.java, { layoutInflater }) { if (setContentView) setContentView(it.root) }
@@ -20,28 +22,55 @@ inline fun <reified VB : ViewBinding> Dialog.viewBinding(setContentView: Boolean
 @MainThread
 inline fun <reified VB : ViewBinding> Fragment.viewBinding() = ViewBindingLazy(VB::class.java, { layoutInflater })
 
-inline fun <reified VB : ViewBinding> bindItemView(view: View) = lazy(LazyThreadSafetyMode.NONE) {
-    VB::class.java.getMethod("bind",View::class.java).invoke(null, view) as VB
-}
+@MainThread
+inline fun <reified VB: ViewBinding> ViewGroup.viewBinding(attachToRoot: Boolean = false) =
+    ViewBindingLazy(VB::class.java, { LayoutInflater.from(context) }, { this }, attachToRoot)
+
+@MainThread
+inline fun <reified VB: ViewBinding> ViewGroup.mergeViewBinding() =
+    MergeViewBindingLazy(VB::class.java, { LayoutInflater.from(context) }, { this })
 
 @Suppress("UNCHECKED_CAST")
 class ViewBindingLazy<VB: ViewBinding>(
     private val viewBindingClass: Class<VB>,
     private val layoutInflater: () -> LayoutInflater,
+    private val root: (() -> ViewGroup)? = null,
+    private val attachToRoot: Boolean = false,
     private val setContentView: ((VB) -> Unit)? = null
 ): Lazy<VB> {
     private var cache: VB? = null
     override val value: VB
         get() {
             return cache ?:
-            (viewBindingClass.getMethod("inflate", LayoutInflater::class.java)
-                .invoke(null, layoutInflater.invoke()) as VB).also {
+            (viewBindingClass.getMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.java)
+                .invoke(null, layoutInflater.invoke(), root?.invoke(), attachToRoot) as VB).also {
                     setContentView?.invoke(it)
                     cache = it
                 }
         }
 
     override fun isInitialized(): Boolean = cache != null
+}
+
+class MergeViewBindingLazy<VB: ViewBinding>(
+    private val viewBindingClass: Class<VB>,
+    private val layoutInflater: () -> LayoutInflater,
+    private val root: (() -> ViewGroup)? = null
+): Lazy<VB> {
+    private var cache: VB? = null
+    override val value: VB
+        get() {
+            return cache ?:
+            (viewBindingClass.getMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java)
+                .invoke(null, layoutInflater.invoke(), root?.invoke()) as VB).also { cache = it }
+        }
+
+    override fun isInitialized(): Boolean = cache != null
+}
+
+@MainThread
+inline fun <reified VB : ViewBinding> bindItemView(view: View) = lazy(LazyThreadSafetyMode.NONE) {
+    VB::class.java.getMethod("bind",View::class.java).invoke(null, view) as VB
 }
 
 @MainThread
